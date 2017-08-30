6bdb35d18dcea7c79ffa8ed54459cfb784308884
gxb <2283478861@qq.com>
2017-08-30 14:41:56 +0800

    Initial commit

---
+++ app/src/main/java/com/gxb/customviewdemo/MyTextView.java
@@ -0,0 +1,45 @@
+package com.gxb.customviewdemo;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by g on 2017/8/30.
+ */
+
+public class MyTextView extends View {
+    private String drawText="";
+    private int textColor;
+    private float textSize;
+    public MyTextView(Context context) {
+        super(context);
+        initAttrs(null);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initAttrs(attrs);
+    }
+
+    private void initAttrs(@Nullable AttributeSet attrs){
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.MyTextView);
+        drawText= typedArray.getString(R.styleable.MyTextView_text);
+        textColor=typedArray.getColor(R.styleable.MyTextView_textColor,0xffffff);
+        textSize=typedArray.getDimension(R.styleable.MyTextView_textSize,20);
+
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(textColor);
+        paint.setTextSize(textSize);
+        canvas.drawText(drawText,10,100,paint);
+    }
+}
